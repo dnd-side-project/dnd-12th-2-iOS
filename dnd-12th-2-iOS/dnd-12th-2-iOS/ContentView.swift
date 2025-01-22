b525ee4ac8cc0c5736f61731c594c1569a7a7b16
Allie <wooyani77@gmail.com>
2025-01-22 15:45:07 +0900

    [SETTING] add project

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by Allie on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/dnd_12th_2_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  dnd_12th_2_iOSApp.swift
+//  dnd-12th-2-iOS
+//
+//  Created by Allie on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct dnd_12th_2_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
