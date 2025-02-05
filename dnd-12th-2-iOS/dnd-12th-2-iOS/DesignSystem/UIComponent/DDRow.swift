1ef2084fe4c13ecb39c33414c462236c3b7f78d4
Allie <wooyani77@gmail.com>
2025-02-06 01:12:31 +0900

    [FEAT] #3 - DDRow 추가

--- dnd-12th-2-iOS/dnd-12th-2-iOS/DesignSystem/UIComponent/DDRow.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/DesignSystem/UIComponent/DDRow.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct DDRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    let title: String
+    let width: CGFloat
+    let height: CGFloat
+    let isSelected: Bool
 
-#Preview {
-    DDRow()
+    init(title: String,
+         width: CGFloat = 343,
+         height: CGFloat = 60,
+         isSelected: Bool = false) {
+        self.title = title
+        self.width = width
+        self.height = height
+        self.isSelected = isSelected
+    }
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.pretendard(size: 16, weight: .medium))
+                .foregroundColor(isSelected ? .purple500 : .gray700)
+            Spacer()
+            if isSelected {
+                Image("iconCheck")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+                    .transition(.opacity)
+            }
+        }
+        .padding(.horizontal, 12)
+        .frame(width: width, height: height)
+        .background(isSelected ? Color.purple50 : Color.gray50)
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(isSelected ? Color.purple500 : Color.clear, lineWidth: 2)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .animation(.easeInOut, value: isSelected)
+    }
 }
