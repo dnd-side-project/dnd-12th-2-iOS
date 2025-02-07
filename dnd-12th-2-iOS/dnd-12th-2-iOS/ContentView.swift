4a3144c5e01dd7cc4ef95bf24d0f3dcfe8a338a4
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-02-07 18:11:01 +0900

    [FEAT] #5 - DDTextField 추가

--- dnd-12th-2-iOS/dnd-12th-2-iOS/ContentView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/ContentView.swift
@@ -8,22 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .font(.system(size: 20))
-            Text("Hello, world!")
-                .font(.pretendard(size: 20, weight: .regular))
-            
-            DDatePicker()
-            DDProgessCard(title: "10키로 감량 다이어트")
-            DDWeekView()
-            DDDscription(content: "실행 장소와 방법을 차분히 생각해보고 정리해보세요.")
-        }
-        .padding()        
+            ScrollView {
+                VStack {
+                    Text("DatePicker")
+                        .font(.title)
+                    DDatePicker()
+                    DDProgessCard(title: "10키로 감량 다이어트")
+                    Text("Progress")
+                        .font(.title)
+                    Text("Calendar")
+                        .font(.title)
+                    DDWeekView()
+                    Text("Description")
+                        .font(.title)
+                    Text("TextField")
+                        .font(.title)
+                    DDTextField(text: $text)
+                    Text("TextField")
+                        .font(.title)
+                    DDDscription(content: "실행 장소와 방법을 차분히 생각해보고 정리해보세요.")
+               
+                }
+                .padding()
+            }
         .navigationBar(left: {
             Image(.appLogo)
         }, right: {
