06cf968afd921d30b17aa38b47c4ca686d40d570
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-02-12 19:19:47 +0900

    [FEAT] #17 - ProfileView 추가

---
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  dnd-12th-2-iOS
+//
+//  Created by 권석기 on 2/12/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
