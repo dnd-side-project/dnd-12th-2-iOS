fa329c9b5ad10dbb4088804a578023abe96961ff
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-02-19 10:59:21 +0900

    [REMOVE] #28 - 메인화면으로 이동하는 임시코드 제거

--- dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Splash/SplashFeature.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/Presentation/Splash/SplashFeature.swift
@@ -30,8 +30,7 @@ struct SplashFeature {
             case .loginCheck:      
                 // 온보딩 완료여부 체크
                 return .run { send in
-                    // 메인으로 이동을 원하시면 해당 주석을 풀어주세요
-//                    let isOnboarding = true
+                    
                     let isOnboarding = try await userClient.fetchUserOnboarding()
                     let isLogin = KeyChainManager.readItem(key: .accessToken) != nil && KeyChainManager.readItem(key: .refreshToken) != nil
                     
