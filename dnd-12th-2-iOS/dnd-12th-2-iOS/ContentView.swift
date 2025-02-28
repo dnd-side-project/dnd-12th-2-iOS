4a889c1c62089ffe01e1edc1839a3f489fc789d4
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-02-28 15:34:59 +0900

    [REMOVE] #36 - 불필요한 파일 삭제

--- dnd-12th-2-iOS/dnd-12th-2-iOS/ContentView.swift
+++ dnd-12th-2-iOS/dnd-12th-2-iOS/ContentView.swift
@@ -9,29 +9,9 @@ import SwiftUI
 import ComposableArchitecture
 
 struct ContentView: View {
-    let store: StoreOf<RootFeature>
     
     var body: some View {
-        SwitchStore(self.store) { state in
-            switch state {
-            case .loginCheck:
-                CaseLet(/RootFeature.State.loginCheck, action: RootFeature.Action.loginCheck) {
-                    SplashView(store: $0)
-                }
-            case .loggedIn:
-                CaseLet(/RootFeature.State.loggedIn, action: RootFeature.Action.loggedIn) {
-                    MainTabView(store: $0)
-                }
-            case .loggedOut:
-                CaseLet(/RootFeature.State.loggedOut, action: RootFeature.Action.loggedOut) {
-                    LoginView(store: $0)                       
-                }
-            case .onboarding:
-                CaseLet(/RootFeature.State.onboarding, action: RootFeature.Action.onboarding) {
-                    OnboardingView(store: $0)
-                }
-            }
-        }
+        Text("test")
     }
 }
 
